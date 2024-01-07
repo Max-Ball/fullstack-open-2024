37f25525f4e987facd1e8d410ceb8545af694bfa
mball <mball@sudscreative.com>
2024-01-07 11:11:31 -0700

    unicafe 1.6-1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,63 +1,104 @@
-const Header = (props) => {
+import { useState } from 'react'
+
+const Header = ({text}) => (
+  <div>
+    <h1>
+      {text}
+    </h1>
+  </div>
+)
+
+const Button = ({text, handleClick}) => {
   return(
-    <div>
-      <h1>{props.course}</h1>
-    </div>
+    <span>
+      <button onClick={handleClick}>{text}</button>
+    </span>
   )
 }
 
-const Content = (props) => {
-  return(
-    <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return(
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <div>
-      <p>Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}</p>
-    </div>
-  )
-}
+const Stats = ({text, number}) => (
+  <div>
+    <span>{text} {number}</span>
+  </div>
+)
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const handleGood = () => {
+    // Set number of good clicks
+    setGood(good + 1)
+
+    // Add to total number of clicks
+    const updatedGood = good + 1;
+    setAll(updatedGood + neutral + bad)
+
+    // Average score (Good: +1; Neutral: 0; Bad: -1)
+    const goodValue = updatedGood
+    const badValue = -(bad)
+    const neutralValue = 0;
+    setAverage((goodValue + badValue + neutralValue) / 3)
+
+
+    // Percentage of positive votes
+    const updatedAll = all + 1;
+    setPositive(updatedGood / updatedAll)
   }
+
+  const handleNeutral = () => {
+    // Set number of neutral clicks
+    setNeutral(neutral + 1)
+
+    // Add to total number of clicks
+    const updatedNeutral = neutral + 1;
+    setAll(good + updatedNeutral + bad)
+
+
+    // Percentage of positive votes
+    const updatedAll = all + 1;
+    setPositive(good / updatedAll)
+  }
+
+  const handleBad = () => {
+    // Set number of bad clicks
+    setBad(bad + 1)
+
+    // Add to total number of clicks
+    const updatedBad = bad + 1;
+    setAll(good + neutral + updatedBad)
+
+    // Average score (Good: +1; Neutral: 0; Bad: -1)
+    const badValue = -(updatedBad)
+    const goodValue = good
+    const neutralValue = 0;
+    setAverage((goodValue + badValue + neutralValue) / 3)
+
+    // Percentage of positive votes
+    const updatedAll = all + 1;
+    setPositive(good / updatedAll)
+  }
+
+  
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total exercise1={course.parts[0].exercises} exercise2={course.parts[1].exercises} exercise3={course.parts[2].exercises} />
+      <Header text={"Give Feedback"} />
+      <Button text="good" handleClick={handleGood} />
+      <Button text="neutral" handleClick={handleNeutral} />
+      <Button text="bad" handleClick={handleBad} />
+      <Header text={"Statistics"} />
+      <Stats text={"good"} number={good} />
+      <Stats text={"neutral"} number={neutral} />
+      <Stats text={"bad"} number={bad} />
+      <Stats text={"all"} number={all} />
+      <Stats text={"average"} number={average} />
+      <Stats text={"positive"} number={positive} />
     </div>
   )
 }
