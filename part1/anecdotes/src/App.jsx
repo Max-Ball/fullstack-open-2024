bf05257c72a89e48a7d3fc49b384b3a7bc085f41
mball <mball@sudscreative.com>
2024-01-07 14:30:41 -0700

    anecdote 1.12-1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,63 +1,48 @@
-const Header = (props) => {
-  return(
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
+import { useState } from 'react'
 
-const Content = (props) => {
-  return(
-    <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return(
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <div>
-      <p>Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}</p>
-    </div>
-  )
-}
-
+const Header = ({title}) => (
+  <div>
+    <h1>{title}</h1>
+  </div>
+)
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const pointArray = Array(anecdotes.length).fill(0)
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(pointArray);
+  const mostPoints = Math.max(...points);
+  const topAnecdote = anecdotes[points.indexOf(mostPoints)]
+
+  const handleClick = (max) => {
+    const amount = Math.floor(Math.random() * max);
+    setSelected(amount); 
+  }
+
+  const handlePoints = (selected) => {
+    const pointsCopy = [...points]
+    pointsCopy[selected] += 1;
+    console.log(pointsCopy);
+    setPoints(pointsCopy)
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total exercise1={course.parts[0].exercises} exercise2={course.parts[1].exercises} exercise3={course.parts[2].exercises} />
+      <button onClick={() => handlePoints(selected)}>vote</button>
+      <button onClick={() => handleClick(anecdotes.length)}>next anecdote</button>
+      <Header title="Anecdote of the Day"/>
+      <div>{anecdotes[selected]}</div>
+      <div>has {points[selected]} votes</div>
+      <Header title="Anecdote with the most votes"/>
+      <span>{topAnecdote}</span>
     </div>
   )
 }
